32731a2e795bf82ccc9b42336d0eeced04794cff
mohamedcharfeddine <mohamed.charfeddine@polytechnicien.tn>
2023-09-21 08:31:41 -0700

    Header is now responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,22 +5,21 @@ const Header = () => {
 		<>
 			<section className="bg-center bg-no-repeat bg-[url('/assets/header.png')] bg-cover">
 				<div className="px-4 mx-auto max-w-screen-xl py-24 lg:py-56">
-					<h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-primary md:text-5xl lg:text-6xl">
+					<h1 className="my-4 text-4xl text-center xl:text-start font-extrabold tracking-tight leading-none text-primary md:text-5xl lg:text-6xl">
 						Taxi Vip Mechelen
 					</h1>
-					<div className="text-white -mt-3 text-xl">
+					<div className="text-white -mt-3 text-xl text-center xl:text-start">
 						Luxurious Transportation
 					</div>
-					<div className="text-white text-2xl py-8 space-y-8">
+					<div className="text-white text-2xl py-8 space-y-8 text-center xl:text-start">
 						Welkom op onze websiteBent u op zoek naar een taxi? Neem gerust
-						<br />
-						contact met ons op!​Wij zijn 24/7 beschikbaar{" "}
+						contact met ons op!​Wij zijn 24/7 beschikbaar
 					</div>
 
-					<div className="flex flex-col space-y-4 sm:flex-row sm:justify-start sm:space-y-0 sm:space-x-4">
+					<div className="flex flex-col xl:flex-row xl:justify-start justify-center items-center ">
 						<a
 							href="#"
-							className="inline-flex justify-start items-center py-3 px-5 text-base font-bold text-center text-dark rounded-full bg-primary">
+							className="inline-flex justify-start items-center py-3 px-5 text-base font-bold text-center text-dark rounded-full bg-primary w-52">
 							Neem Contact op
 						</a>
 						<a
