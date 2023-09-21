99bad7b40f55082e477a196059b94f2dc0e1963a
mohamedcharfeddine <mohamed.charfeddine@polytechnicien.tn>
2023-09-21 08:30:47 -0700

    about section is now responsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -10,8 +10,9 @@ const About = () => {
 					<SiGooglepodcasts size={32} />
 					<span className="text-2xl font-bold">About Us</span>
 				</div>
-				<div className="flex justify-center items-center gap-28 text-justify mt-14">
-					<p className="w-2/5 -ml-10 font-medium text-base/loose text-secondary">
+				<div className="flex justify-center items-center gap-28 text-justify mt-14 flex-col xl:flex-row">
+
+					<p className="xl:w-2/5 mx-12 xl:-ml-10 font-medium text-base/loose text-secondary">
 						Taxi Vip Mechelen rijdt voor particulieren en bedrijven in regio
 						Mechelen en omliggende steden. Tevens bieden wij ook vervoer van en
 						naar de nationale en internationale luchthavens. Contacteer ons op
@@ -23,9 +24,9 @@ const About = () => {
 						staan. 365 dagen per jaar, 24 uur op 24.
 					</p>
 
-					<div class="relative flex h-[500px] w-[500px]  rounded-3xl bg-primary/50 justify-end before:absolute">
+					<div class="relative flex xl:h-[500px] xl:w-[500px] w-[250px] h-[250px] xl:rounded-3xl rounded-xl bg-primary/50 justify-end before:absolute">
 						<Image
-							class="z-10 h-[500px] mr-6 -mt-5"
+							class="z-10 xl:mr-6 -mt-5 mr-5 mb-5 "
 							src="/assets/about.png"
 							width={500}
 							height={500}
